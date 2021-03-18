faf34ff0e75795c6cc9fb5760dcb650d7a3f8fdd
savuth ven <savuthdev2020@gmail.com>
2021-03-18 16:11:05 +0700

    product-component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 //branch feature1
-import React from 'react';
-import './index.css';
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="">Shopping Cart</a>
-      </header>
-
-      <main>
-        Product list
-      </main>
-      <footer>
-        All right is reserve
-      </footer>
-    </div>
-  )
+import React from "react";
+import "./index.css";
+import data from "./data.json";
+import Products from "./components/Products";
+class App extends React.Component {
+  constructor() {
+    super(),
+    this.state = {
+      products:data.products,
+      size:"",
+      sort:""
+    }
+  }
+  render() {
+    // console.log(this.state.products);
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="">Shopping Cart</a>
+        </header>
+  
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">All Cart Items</div>
+          </div>
+        </main>
+        <footer>
+          All right is reserve
+        </footer>
+      </div>
+    )
+  }
 }
 export default App;
