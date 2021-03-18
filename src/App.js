0e5f0899e510164a124f320a848adbbac02d291a
savuth ven <savuthdev2020@gmail.com>
2021-03-18 14:42:52 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="">Shopping Cart</a>
+      </header>
+
+      <main>
+        Product list
+      </main>
+      <footer>
+        All right is reserve
+      </footer>
+    </div>
+  )
+}
+export default App;
